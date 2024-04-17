de4d2e167a1e01fadd9ba79325298cafa0381c9d
Ramya krishna <ramyakrishnaimminni@gmail.com>
2024-04-17 13:01:00 +0530

    updating api key to empty

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 use NewfoldLabs\WP\Module\Migration\Migration;
-use NewfoldLabs\WP\Module\Migration\Services\UtilityService;
+// use NewfoldLabs\WP\Module\Migration\Services\UtilityService;
 use NewfoldLabs\WP\ModuleLoader\Container;
 
 use function NewfoldLabs\WP\ModuleLoader\register;
@@ -27,7 +27,7 @@ if ( function_exists( 'add_action' ) ) {
 						defined( 'INSTAWP_API_KEY' ) || define( 'INSTAWP_API_KEY', '' );
 						defined( 'INSTAWP_API_DOMAIN' ) || define( 'INSTAWP_API_DOMAIN', 'https://app.instawp.io' );
 						defined( 'INSTAWP_MIGRATE_ENDPOINT' ) || define( 'INSTAWP_MIGRATE_ENDPOINT', 'migrate/bluehost' );
-						$response = UtilityService::get_insta_api_key();
+						// $response = UtilityService::get_insta_api_key();
 						// defined( 'INSTAWP_API_KEY' ) || define( 'INSTAWP_API_KEY', $response );
 
 						new Migration( $container );
