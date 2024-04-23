345c8d7d379d1cc4b9ca5c044eecde186b407cb7
Ramya krishna <ramyakrishnaimminni@gmail.com>
2024-04-23 17:47:32 +0530

    translations added to js files

--- src/components/DashboardMigrate.js
+++ src/components/DashboardMigrate.js
@@ -1,34 +1,35 @@
 import { ArrowLongRightIcon } from '@heroicons/react/24/outline';
+import { __ } from "@wordpress/i18n";
+
+const content = {
+	title: __( "Let's migrate your existing site to your new account", "wp-module-migration" ),
+	description: __( "Migrating your site is easy with our free site import tool. Or, you can pay to have our Migrations Team experts handle your transfer for you.", "wp-module-migration" ),
+	learnMoreText: __( "Learn more", "wp-module-migration" ),
+	dismissButtonText: __( "Dismiss this card", "wp-module-migration" ),
+	buttonText: __( "Migrate your site", "wp-module-migration" )
+}
 
 const DashboardMigrate = ( {} ) => {
 	const connectMigrate = () => {
-		// eslint-disable-next-line no-console
-		getMigrateRedirectUrl().then( ( res ) => {
-			// eslint-disable-next-line no-console
+		getMigrateRedirectUrl().then( (res) => {
 			window.open( res?.data?.redirect_url, '_self' );
-		} );
+		});
 	};
 	return (
 		<div className=" nfd-rounded nfd-border nfd-p-4 nfd-grid nfd-grid-cols-2 nfd-gap-4">
 			<div className="nfd-grid nfd-grid-rows-[repeat(2,_min-content)] nfd-gap-6">
 				<div>
 					<h1 className="nfd-title nfd-title--2">
-						{
-							"	Let's migrate your existing site to your new account"
-						}
+						{ content.title }
 					</h1>
 					<p>
-						Migrating your site is easy with our free site import
-						tool. Or, you can pay to have our Migrations Team
-						experts handle your transfer for you.{ ' ' }
-						<a href="testing" target="_blank">
-							Learn more
-						</a>
-						.
+						<a href="#" target="_blank">
+							{ content.learnMoreText }
+						</a>.
 					</p>
 				</div>
 				<button className="nfd-text-primary nfd-text-left">
-					Dismiss this card
+					{ content.dismissButtonText }
 				</button>
 			</div>
 			<div className="nfd-flex nfd-items-center nfd-justify-end">
@@ -37,7 +38,7 @@ const DashboardMigrate = ( {} ) => {
 					onClick={ () => connectMigrate() }
 					className="nfd-button nfd-button--primary nfd-flex nfd-gap-2"
 				>
-					Migrate your site{ ' ' }
+					{ content.buttonText }
 					<ArrowLongRightIcon className="nfd-w-[1.125rem]" />
 				</button>
 			</div>
