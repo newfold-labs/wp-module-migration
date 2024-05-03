00418eb300b3fd322458a3d1114bc2e0804acdae
Ramya krishna <ramyakrishnaimminni@gmail.com>
2024-05-03 12:15:02 +0530

    version bump

--- bootstrap.php
+++ bootstrap.php
@@ -23,7 +23,7 @@ if ( function_exists( 'add_action' ) ) {
 					'callback' => function ( Container $container ) {
 
 						if ( ! defined( 'NFD_MIGRATION_MODULE_VERSION' ) ) {
-							define( 'NFD_MIGRATION_MODULE_VERSION', '1.0.0' );
+							define( 'NFD_MIGRATION_MODULE_VERSION', '1.0.1' );
 						}
 						$brand = $container->plugin()->id;
 						if ( 'atomic' === getContext( 'platform' ) ) {
