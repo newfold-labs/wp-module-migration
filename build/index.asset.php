4e0acddd404de4660d2707b67200fc0c6ca50ee2
Evan Mullins <evanrm@gmail.com>
2024-06-19 18:09:29 -0400

    updated build file

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-i18n'), 'version' => '5984a9e5a2eec310162a');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-i18n'), 'version' => 'f41b874a5c7024d964a3');
