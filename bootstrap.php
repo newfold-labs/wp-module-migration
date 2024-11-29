68ffa4d9748120e0c22c4e5eda25c337fbbdaf60
Alessio Torrisi <alessiotorrisidev@gmail.com>
2024-11-29 15:10:38 +0100

    Tweak: create instance for Migration class only in case of Bluehost and Hostgator brand plugins

--- bootstrap.php
+++ bootstrap.php
@@ -21,27 +21,28 @@ if ( function_exists( 'add_action' ) ) {
 					'label'    => __( 'Migration', 'wp-module-migration' ),
 					'callback' => function ( Container $container ) {
 
-						if ( ! defined( 'NFD_MIGRATION_MODULE_VERSION' ) ) {
-							define( 'NFD_MIGRATION_MODULE_VERSION', '1.0.12' );
+						if ( in_array( $container->plugin()->id, array( 'bluehost', 'hostgator' ) ) ) {
+							if ( ! defined( 'NFD_MIGRATION_MODULE_VERSION' ) ) {
+								define( 'NFD_MIGRATION_MODULE_VERSION', '1.0.12' );
+							}
+							$brand = $container->plugin()->id;
+
+							$migrate_brand = $brand;
+
+							if ( 'atomic' === getContext( 'platform' ) ) {
+								$brand = 'bh-cloud';
+							}
+							define( 'NFD_MIGRATION_PLUGIN_URL', $container->plugin()->url );
+
+							defined( 'NFD_PROXY_ACCESS_WORKER' ) || define( 'NFD_PROXY_ACCESS_WORKER', 'https://hiive.cloud/workers/migration-token-proxy' );
+							defined( 'NFD_MIGRATION_PROXY_WORKER' ) || define( 'NFD_MIGRATION_PROXY_WORKER', 'https://migrate.' . $migrate_brand . '.com' );
+
+							defined( 'BRAND_PLUGIN' ) || define( 'BRAND_PLUGIN', $brand );
+
+							defined( 'INSTAWP_MIGRATE_ENDPOINT' ) || define( 'INSTAWP_MIGRATE_ENDPOINT', 'migrate/' . $brand );
+
+							new Migration( $container );
 						}
-						$brand = $container->plugin()->id;
-
-						$migrate_brand = $brand;
-
-						if ( 'atomic' === getContext( 'platform' ) ) {
-							$brand = 'bh-cloud';
-							$migrate_brand = 'bluehost';
-						}
-						define( 'NFD_MIGRATION_PLUGIN_URL', $container->plugin()->url );
-
-						defined( 'NFD_PROXY_ACCESS_WORKER' ) || define( 'NFD_PROXY_ACCESS_WORKER', 'https://hiive.cloud/workers/migration-token-proxy' );
-						defined( 'NFD_MIGRATION_PROXY_WORKER' ) || define( 'NFD_MIGRATION_PROXY_WORKER', 'https://migrate.' . $migrate_brand . '.com' );
-
-						defined( 'BRAND_PLUGIN' ) || define( 'BRAND_PLUGIN', $brand );
-
-						defined( 'INSTAWP_MIGRATE_ENDPOINT' ) || define( 'INSTAWP_MIGRATE_ENDPOINT', 'migrate/' . $brand );
-
-						new Migration( $container );
 					},
 					'isActive' => true,
 					'isHidden' => true,
--- includes/Migration.php
+++ includes/Migration.php
@@ -61,10 +61,8 @@ class Migration {
 		add_action( 'rest_api_init', array( $this, 'register_routes' ) );
 		add_action( 'pre_update_option_nfd_migrate_site', array( $this, 'on_update_nfd_migrate_site' ) );
 		add_action( 'pre_update_option_instawp_last_migration_details', array( $this, 'on_update_instawp_last_migration_details' ), 10, 1 );
-		if ( in_array( $container->plugin()->id, array( 'bluehost', 'hostgator' ) ) ) {
-			add_action( 'load-import.php', array( $this, 'register_wp_migration_tool' ) ); // Adds WordPress Migration tool to imports list
-			add_action( 'admin_enqueue_scripts', array( $this, 'set_import_tools' ) );
-		}
+		add_action( 'load-import.php', array( $this, 'register_wp_migration_tool' ) ); // Adds WordPress Migration tool to imports list.
+		add_action( 'admin_enqueue_scripts', array( $this, 'set_import_tools' ) );
 	}
 
 	/**
