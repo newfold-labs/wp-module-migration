fce5b68d085899ca5e3e911784903cbe601a7770
Evan Mullins <evanrm@gmail.com>
2025-03-07 14:10:05 -0500

    Version updated 1.2.0 => 1.2.1

--- bootstrap.php
+++ bootstrap.php
@@ -22,7 +22,7 @@ if ( function_exists( 'add_action' ) ) {
 					'callback' => function ( Container $container ) {
 
 						if ( ! defined( 'NFD_MIGRATION_MODULE_VERSION' ) ) {
-							define( 'NFD_MIGRATION_MODULE_VERSION', '1.2.0' );
+							define( 'NFD_MIGRATION_MODULE_VERSION', '1.2.1' );
 						}
 						$brand = $container->plugin()->id;
 						if ( 'atomic' === getContext( 'platform' ) ) {
