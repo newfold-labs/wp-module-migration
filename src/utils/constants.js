fef132c3dab03179488eff4c2e224446676ff37a
Evan Mullins <evanrm@gmail.com>
2025-03-10 18:09:29 -0400

    prep version update and refresh build 1.2.2
    update reference to newfold runtime pacakge use

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-import { NewfoldRuntime } from '@newfold-labs/wp-module-runtime';
+import { NewfoldRuntime } from '@newfold/wp-module-runtime';
 
 export const MIGRATE_CONNECT = NewfoldRuntime.createApiUrl(
 	'/newfold-migrate/v1/migrate/connect'
