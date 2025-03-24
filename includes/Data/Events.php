4dc705d058b644b7fffc763b209f0d3ea6a59e6b
Armando Liccardo <li.armando82@gmail.com>
2025-03-24 13:03:57 +0000

    changed category of event to simply Wonder_Start

--- includes/Data/Events.php
+++ includes/Data/Events.php
@@ -10,7 +10,7 @@ final class Events {
 	 *
 	 * @var array
 	 */
-	protected static $category = array( 'WP\Module\Migration\Listeners\Wonder_Start' );
+	protected static $category = array( 'Wonder_Start' );
 
 	/**
 	 * List of valid actions that an event can perform.
