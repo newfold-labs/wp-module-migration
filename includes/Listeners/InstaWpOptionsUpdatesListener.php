9fcca5ee6d2290c6fd0a7ab8a0ab423426c7b0e3
Armando Liccardo <li.armando82@gmail.com>
2025-03-25 13:29:31 +0000

    refactoring the instawpoptionlistener

--- includes/Listeners/InstaWpOptionsUpdatesListener.php
+++ includes/Listeners/InstaWpOptionsUpdatesListener.php
@@ -36,7 +36,6 @@ class InstaWpOptionsUpdatesListener {
 		$this->tracker = new Tracker();
 		add_filter( 'pre_update_option_instawp_last_migration_details', array( $this, 'on_update_instawp_last_migration_details' ), 10, 2 );
 		add_filter( 'pre_update_option_instawp_migration_details', array( $this, 'on_update_instawp_migration_details' ), 10, 2 );
-		add_action( 'add_option_instawp_last_migration_details', array( $this, 'after_migration_steps' ), 10, 2 );
 	}
 	/**
 	 * Push event with tracking file content.
@@ -62,29 +61,66 @@ class InstaWpOptionsUpdatesListener {
 	 * @param array $old_value previous status of migration
 	 */
 	public function on_update_instawp_last_migration_details( $new_value, $old_value ) {
-		return $new_value;
 		if ( $old_value !== $new_value ) {
-			$value_updated = $new_value['status'];
-			if ( 'completed' === $value_updated || 'failed' === $value_updated || 'aborted' === $value_updated ) {
-				$push = new Push();
-				$push->set_status( $push->statuses['completed'] );
-				$this->tracker->update_track( $push );
-			}
-			if ( 'completed' === $value_updated ) {
-				$migration_complete = new LastStep();
-				$migration_complete->set_status( $migration_complete->statuses['completed'] );
-				$this->tracker->update_track( $migration_complete );
-				$this->push( 'migration_completed', $this->tracker->get_track_content() );
-			} elseif ( 'failed' === $value_updated ) {
-				$migration_complete = new LastStep();
-				$migration_complete->set_status( $migration_complete->statuses['failed'] );
-				$this->tracker->update_track( $migration_complete );
-				$this->push( 'migration_failed', $this->tracker->get_track_content() );
-			} elseif ( 'aborted' === $value_updated ) {
-				$migration_complete = new LastStep();
-				$migration_complete->set_status( $migration_complete->statuses['aborted'] );
-				$this->tracker->update_track( $migration_complete );
-				$this->push( 'migration_aborted', $this->tracker->get_track_content() );
+			$migrate_group_uuid = isset( $new_value['migrate_group_uuid'] ) ? $new_value['migrate_group_uuid'] : '';
+			if ( ! empty( $migrate_group_uuid ) ) {
+				$token = UtilityService::get_insta_api_key( BRAND_PLUGIN );
+				if ( $token && $migrate_group_uuid ) {
+					error_log( 'calling instawp api' );
+					$response = wp_remote_get(
+						'https://app.instawp.io/api/v2/migrates-v3/status/' . $migrate_group_uuid,
+						array(
+							'headers' => array(
+								'Authorization' => 'Bearer ' . $token,
+							)
+						)
+					);
+
+					if ( wp_remote_retrieve_response_code( $response ) === 200 && ! is_wp_error( $response ) ) {
+						$body = wp_remote_retrieve_body( $response );
+						$data = json_decode( $body, true );
+						if ( $data && is_array( $data ) && isset( $data['status'] ) && $data['status'] ) {
+							$migration_status = $data['data']['status'];
+
+							if ( 'completed' === $migration_status || 'failed' === $migration_status || 'aborted' === $migration_status ) {
+								$push = new Push();
+								$push->set_status( $push->statuses['completed'] );
+								$this->tracker->update_track( $push );
+							}
+
+							if ( isset( $data['data']['source_site_url'] ) ) {
+								$source_site_url = $data['data']['source_site_url'];
+	
+								error_log( 'get source url' );
+								//domain_host
+								//speedindex source site
+								//speedindex destination site
+							}
+				
+							if ( 'completed' === $migration_status ) {
+								$migration_complete = new LastStep();
+								$migration_complete->set_status( $migration_complete->statuses['completed'] );
+								$this->tracker->update_track( $migration_complete );
+								$this->push( 'migration_completed', $this->tracker->get_track_content() );
+							} elseif ( 'failed' === $migration_status ) {
+								$migration_complete = new LastStep();
+								$migration_complete->set_status( $migration_complete->statuses['failed'] );
+								$this->tracker->update_track( $migration_complete );
+								$this->push( 'migration_failed', $this->tracker->get_track_content() );
+							} elseif ( 'aborted' === $migration_status ) {
+								$migration_complete = new LastStep();
+								$migration_complete->set_status( $migration_complete->statuses['aborted'] );
+								$this->tracker->update_track( $migration_complete );
+								$this->push( 'migration_aborted', $this->tracker->get_track_content() );
+							}
+							
+						} else {
+							error_log( 'Error decoding response: ' . json_last_error_msg() );
+						}
+					} else {
+						error_log( 'Error in response: ' . $response->get_error_message() );
+					}
+				}
 			}
 		}
 
@@ -133,8 +169,8 @@ class InstaWpOptionsUpdatesListener {
 							$source_site_url = $data['data']['source_site_url'];
 
 							//speedindex source site
-							//domainhost
 							//speedindex destination site
+							$source_url_pagespeed = new PageSpeed( $source_site_url, 'source' );
 
 						}
 					} else {
