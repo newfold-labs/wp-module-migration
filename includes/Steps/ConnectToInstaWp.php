b4db2febe777b3a4000e12fdce29f7644c6e507f
arunshenoy99 <devarunshenoy99@gmail.com>
2025-06-20 13:25:30 +0530

    Track E2E migration in InstaWP

--- includes/Steps/ConnectToInstaWp.php
+++ includes/Steps/ConnectToInstaWp.php
@@ -19,6 +19,14 @@ class ConnectToInstaWp extends AbstractStep {
 	private $insta_api_key = '';
 
 	/**
+	 * The current brand/plugin identifier, used when sending the white-label slug to InstaWP.
+	 * Defaults to 'bluehost' if the BRAND_PLUGIN constant is not defined.
+	 *
+	 * @var string
+	 */
+	public $brand = '';
+
+	/**
 	 * Construct. Init basic parameters.
 	 *
 	 * @param string $insta_api_key instawp api key.
@@ -27,6 +35,11 @@ class ConnectToInstaWp extends AbstractStep {
 		$this->set_step_slug( 'ConnectToInstaWp' );
 		$this->set_max_retries( 2 );
 		$this->insta_api_key = $insta_api_key;
+		if ( defined( 'BRAND_PLUGIN' ) ) {
+			$this->brand = BRAND_PLUGIN;
+		} else {
+			$this->brand = 'bluehost';
+		}
 		$this->set_status( $this->statuses['running'] );
 		$this->run();
 	}
@@ -39,7 +52,14 @@ class ConnectToInstaWp extends AbstractStep {
 	protected function run() {
 		if ( empty( Helper::get_api_key() ) || empty( Helper::get_connect_id() ) ) {
 			$api_key          = Helper::get_api_key( false, $this->insta_api_key );
-			$connect_response = Helper::instawp_generate_api_key( $api_key, '', false );
+			$connect_response = Helper::instawp_generate_api_key(
+				$api_key,
+				'',
+				array(
+					'e2e_mig_push_request' => true,
+					'wlm_slug'             => $this->brand,
+				)
+			);
 			if ( ! $connect_response ) {
 				delete_option( 'instawp_api_key' );
 				if ( ! $this->retry() ) {
